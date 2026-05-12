0becbd56796b81badff19207c3153a63d5f63022
Sumit1694 <Sumitrane198@gmail.com>
2026-05-13 04:11:13 +0530

    Bit manipulation approach for Even and odd Count

--- src/Arrays/EvenAndOddNumbersCount.java
+++ src/Arrays/EvenAndOddNumbersCount.java
@@ -42,11 +42,32 @@ public class EvenAndOddNumbersCount {
 		System.out.println("Even count: " + evenCount + " Odd count: " + oddCount);
 	}
 
+	static void EvenAndOddCount3(int [] arr)
+	{
+		int evenCount = 0;
+		int oddCount = 0;
+
+		for(int i:arr)
+		{
+			if((i&1)==0)
+			{
+				evenCount++;
+			}
+			else
+			{
+				oddCount++;
+			}
+		}
+
+		System.out.println("Even count: " + evenCount + " Odd count: " + oddCount);
+	}
+
 	public static void main(String[] args) {
 
 		int [] arr = {2,5,8,11,14};
         EvenAndOddCount1(arr);
         EvenAndOddCount2(arr);
+        EvenAndOddCount3(arr);
 	}
 
 }
