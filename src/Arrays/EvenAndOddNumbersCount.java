2ea6a5813159b748c89b9c19344fd790ea1cab33
Sumit1694 <Sumitrane198@gmail.com>
2026-05-13 04:09:24 +0530

    Even and odd count with enhanced for loop

--- src/Arrays/EvenAndOddNumbersCount.java
+++ src/Arrays/EvenAndOddNumbersCount.java
@@ -22,10 +22,31 @@ public class EvenAndOddNumbersCount {
 		System.out.println("Even count: " + evenCount + " Odd count: " + oddCount);
 	}
 
+	static void EvenAndOddCount2(int [] arr)
+	{
+		int evenCount = 0;
+		int oddCount = 0;
+
+		for(int i:arr)
+		{
+			if(i%2==0)
+			{
+				evenCount++;
+			}
+			else
+			{
+				oddCount++;
+			}
+		}
+
+		System.out.println("Even count: " + evenCount + " Odd count: " + oddCount);
+	}
+
 	public static void main(String[] args) {
 
 		int [] arr = {2,5,8,11,14};
         EvenAndOddCount1(arr);
+        EvenAndOddCount2(arr);
 	}
 
 }
