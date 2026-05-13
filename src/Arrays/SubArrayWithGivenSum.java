615cd5868b695f3239b9465274a98017db840dc0
Sumit1694 <Sumitrane198@gmail.com>
2026-05-14 00:48:19 +0530

    SubArraywith given sum big(n)

--- src/Arrays/SubArrayWithGivenSum.java
+++ src/Arrays/SubArrayWithGivenSum.java
@@ -26,10 +26,39 @@ public class SubArrayWithGivenSum {
 		System.out.println("No subarray found!");
 	}
 
+	static void subArrayWithSum1(int [] arr, int target)
+	{
+		int i=0;
+		int currSum = 0;
+
+		for(int j=0;j<arr.length;j++)
+		{
+			currSum += arr[j];
+
+			while(currSum>target && i<=j)
+			{
+				currSum -= arr[i];
+				i++;
+			}
+
+			if(currSum==target)
+			{
+				System.out.println("Subarray: ");
+
+				for(int k=i;k<=j;k++)
+				{
+					System.out.print(arr[k] +" ");
+				}
+				return;
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 
 		int [] arr = {1, 4, 20, 3, 10, 5};
-		subArrayWithSum(arr, 33);
+		//subArrayWithSum(arr, 33);
+		subArrayWithSum1(arr, 33);
 
 	}
 
