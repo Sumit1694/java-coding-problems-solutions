19d9cef4e18d55a7d23dd8847194c0122b5f2110
Sumit1694 <Sumitrane198@gmail.com>
2026-05-13 20:21:49 +0530

    MaxsumSubarray kadanes Algorithm

--- src/Arrays/MaximumSubArraySum.java
+++ src/Arrays/MaximumSubArraySum.java
@@ -23,11 +23,35 @@ public class MaximumSubArraySum {
 		return maxSum;
 	}
 
+	static int KadanesAlgorithm(int [] arr)
+	{
+		int maxSum = arr[0];
+		int sum = arr[0];
+
+		for(int i=1;i<arr.length;i++)
+		{
+			if(sum>0)
+			{
+				sum += arr[i];
+			}
+			if(sum<0)
+			{
+				sum = arr[i];
+			}
+
+			if(sum>maxSum)
+			{
+				maxSum = sum;
+			}
+		}
+		return maxSum;
+	}
+
 	public static void main(String[] args) {
 
 		int [] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
 		System.out.println(maxSumSubArray(arr));
-
+        System.out.println(KadanesAlgorithm(arr));
 	}
 
 }
