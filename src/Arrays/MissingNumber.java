8ebd4b275877ca5fee86c2e02aa152259e990b0b
Sumit1694 <Sumitrane198@gmail.com>
2026-05-13 20:43:24 +0530

    Missing number

--- src/Arrays/MissingNumber.java
+++ src/Arrays/MissingNumber.java
@@ -16,10 +16,30 @@ public class MissingNumber {
 		return expSum - actSum;
 	}
 
+	static int missingNumber1(int [] arr)
+	{
+		int n = arr.length+1;
+		int actSum = 0;
+		int expSum = 0;
+
+		for(int i=0;i<arr.length;i++)
+		{
+			actSum += arr[i];
+		}
+
+		for(int i=0;i<=n;i++)
+		{
+			expSum += i;
+		}
+
+		return expSum - actSum;
+	}
+
 	public static void main(String[] args) {
 
 		int [] arr = {1,2,4,5,6};
 		System.out.println(missingNumber(arr));
+		System.out.println(missingNumber1(arr));
 	}
 
 }
