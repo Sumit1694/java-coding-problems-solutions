49ed8bdb90ed0b3ce656f855782ef75676b07b4f
Sumit1694 <Sumitrane198@gmail.com>
2026-05-15 03:58:06 +0530

    all divisors function

---
+++ src/Numbers/AllDivisors.java
@@ -0,0 +1,29 @@
+package Numbers;
+
+import java.util.Scanner;
+
+public class AllDivisors {
+
+	static void allDivisors(int n)
+	{
+		for(int i=1;i<=n;i++)
+		{
+			if(n%i==0)
+			{
+				System.out.print(i+" ");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int n = sc.nextInt();
+		allDivisors(n);
+		sc.close();
+
+
+	}
+
+}
