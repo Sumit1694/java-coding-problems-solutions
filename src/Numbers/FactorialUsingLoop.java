e821efbc62321216929854b94f1206409053af9c
Sumit1694 <Sumitrane198@gmail.com>
2026-05-14 21:41:05 +0530

    Factorial of number using loop

---
+++ src/Numbers/FactorialUsingLoop.java
@@ -0,0 +1,22 @@
+package Numbers;
+
+public class FactorialUsingLoop {
+
+	static int factorial(int n)
+	{
+		int res = 1;
+
+		for(int i=1;i<=n;i++)
+		{
+			res *= i;
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+
+		int num = 5;
+		System.out.println(factorial(num));
+	}
+
+}
