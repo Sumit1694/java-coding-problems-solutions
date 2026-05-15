cc6640ffffbd8b8e29cb47ae82e4e4f811a1048b
Sumit1694 <Sumitrane198@gmail.com>
2026-05-15 19:28:29 +0530

    Move even elements to left and odd to the right side

---
+++ src/Arrays/EvenLeftAndOddRight.java
@@ -0,0 +1,57 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class EvenLeftAndOddRight {
+
+	static void moveEvenAndOdd(int [] arr)
+	{
+		int l=0, h=arr.length-1;
+
+		while(l<=h)
+		{
+			if(arr[l]%2==0)
+			{
+				l++;
+			}
+			else if(arr[h]%2!=0)
+			{
+				h--;
+			}
+			else
+			{
+				int temp = arr[l];
+				arr[l] = arr[h];
+				arr[h] = temp;
+				l++;
+				h--;
+			}
+		}
+	}
+
+	static void moveEvenOrOdd(int [] arr)
+	{
+		int e = 0;
+
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]%2==0)
+			{
+				int temp = arr[i];
+				arr[i] = arr[e];
+				arr[e] = temp;
+				e++;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int [] arr = {1,4,3,8,7,2};
+		int [] arr2 = {2, 4, 1, 6};
+		//moveEvenAndOdd(arr);
+		moveEvenOrOdd(arr2);
+		System.out.println(Arrays.toString(arr2));
+	}
+
+}
