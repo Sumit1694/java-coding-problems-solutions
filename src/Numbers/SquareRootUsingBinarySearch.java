8e2bb3486657a616492eac725309bd4b2f0ef6b2
Sumit1694 <Sumitrane198@gmail.com>
2026-05-15 09:17:27 +0530

    Square root function

---
+++ src/Numbers/SquareRootUsingBinarySearch.java
@@ -0,0 +1,43 @@
+package Numbers;
+
+public class SquareRootUsingBinarySearch {
+
+	static int sqrt(int n)
+	{
+		if(n==1 || n==0)
+		{
+			return n;
+		}
+
+		int l=2, h=n/2;
+		int res = 0;
+
+		while(l<=h)
+		{
+			int m = l + (h-l)/2;
+
+			if(m*m==n)
+			{
+				return m;
+			}
+			else if(m*m<n)
+			{
+				l = m + 1;
+				res = m;
+			}
+			else
+			{
+				h = m - 1;
+			}
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+
+		int n = 24;
+		System.out.println(sqrt(n));
+
+	}
+
+}
