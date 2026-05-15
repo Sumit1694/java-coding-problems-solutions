e41bbbd7b4be5e41d7df99b7067bb05e0645a637
Sumit1694 <Sumitrane198@gmail.com>
2026-05-16 02:12:50 +0530

    Swapping without temp

---
+++ src/Numbers/SwapWithoutTemp.java
@@ -0,0 +1,20 @@
+package Numbers;
+
+public class SwapWithoutTemp {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+
+		System.out.println("Before Swapping: a = " + a +" b = " + b);
+
+		a = a + b; //30
+		b = a - b; //10
+		a = a - b; //20
+
+		System.out.println("After Swapping: a = " + a +" b = " + b);
+
+	}
+
+}
