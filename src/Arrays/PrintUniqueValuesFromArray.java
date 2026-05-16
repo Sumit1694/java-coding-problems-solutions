84f3fda4e3172c1e1a386c182b6e6c1a1697b400
Sumit1694 <Sumitrane198@gmail.com>
2026-05-17 01:40:37 +0530

    print Every element one time only

---
+++ src/Arrays/PrintUniqueValuesFromArray.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+public class PrintUniqueValuesFromArray {
+
+	static void unique(int [] arr)
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			boolean found = false;
+
+			for(int j=0;j<i;j++)
+			{
+				if(arr[i]==arr[j])
+				{
+					found = true;
+					break;
+				}
+			}
+
+			if(!found)
+			{
+				System.out.print(arr[i] + " ");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int [] arr = {1,2,2,3,1,4};
+		unique(arr);
+
+	}
+
+}
