7fd2682a21651c482076ffb24b6d4438f9f4083e
Sumit1694 <Sumitrane198@gmail.com>
2026-05-16 13:26:24 +0530

    String expansion

---
+++ src/Recursion/StringExpanstion.java
@@ -0,0 +1,32 @@
+package Recursion;
+
+public class StringExpanstion {
+
+	static void expand(String str,int index)
+	{
+		if(index>=str.length())
+		{
+			return;
+		}
+
+		char ch = str.charAt(index);
+
+		int count = str.charAt(index+1)-'0';
+
+		while(count>0)
+		{
+			System.out.print(ch);
+			count--;
+		}
+
+		expand(str, index+2);
+	}
+
+	public static void main(String[] args) {
+
+		String str = "a2b3c4";
+		expand(str, 0);
+
+	}
+
+}
