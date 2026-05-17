24a3dd5c0a5d2107f01e48698bd8df548fa0bd22
Sumit1694 <Sumitrane198@gmail.com>
2026-05-17 14:42:29 +0530

    Longest substring without repeating chars

--- src/Strings/LongestSubStringWithoutRepeatingChars.java
+++ src/Strings/LongestSubStringWithoutRepeatingChars.java
@@ -33,9 +33,30 @@ public class LongestSubStringWithoutRepeatingChars {
 		return longest;
 	}
 
+	static int lengthOfLongestSubstring(String str)
+	{
+		Set<Character>set = new HashSet<>();
+		int maxLength = 0;
+		int left = 0;
+
+		for(int right=0;right<str.length();right++)
+		{
+			while(set.contains(str.charAt(right)))
+			{
+				set.remove(str.charAt(left));
+				left++;
+			}
+			set.add(str.charAt(right));
+			maxLength = Math.max(maxLength, right-left+1);
+		}
+
+		return maxLength;
+	}
+
 	public static void main(String[] args) {
 
 		String str = "abcabcbb";
 		System.out.println(longest(str));
+		System.out.println(lengthOfLongestSubstring(str));
 	}
 }
