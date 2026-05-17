37440770aa957407d9a3b7e6775d4ddbc730899b
Sumit1694 <Sumitrane198@gmail.com>
2026-05-17 23:20:56 +0530

    Reverse String while preserving spaces

---
+++ src/Strings/ReverseStringWhilePreservingSpaces.java
@@ -0,0 +1,36 @@
+package Strings;
+
+public class ReverseStringWhilePreservingSpaces {
+
+	public static void main(String[] args) {
+
+		String str = "i am boy";
+		String noSpace = str.replaceAll(" ", "");
+		String rev = "";
+		for(int i=noSpace.length()-1;i>=0;i--)
+		{
+			rev += noSpace.charAt(i);
+		}
+
+		String result = "";
+		int index = 0;
+
+		for(int j=0;j<str.length();j++)
+		{
+			char ch = str.charAt(j);
+
+			if(str.charAt(j)==' ')
+			{
+				result += " ";
+			}
+			else
+			{
+				result += rev.charAt(index);
+				index++;
+			}
+		}
+
+		System.out.println(result);
+	}
+
+}
