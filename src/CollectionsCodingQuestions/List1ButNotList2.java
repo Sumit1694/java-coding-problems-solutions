6f88d195343b08623b4efa18c9a84c381d740c42
Sumit1694 <Sumitrane198@gmail.com>
2026-06-17 18:24:07 +0530

    Employees present in list1 but not in list2

---
+++ src/CollectionsCodingQuestions/List1ButNotList2.java
@@ -0,0 +1,19 @@
+package CollectionsCodingQuestions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class List1ButNotList2 {
+
+	public static void main(String[] args) {
+
+		List<Integer> list1 = new ArrayList<>(Arrays.asList(101,102,103));
+		List<Integer> list2 = new ArrayList<>(Arrays.asList(102));
+
+		list1.removeAll(list2);
+
+		System.out.println(list1);
+
+	}
+}
