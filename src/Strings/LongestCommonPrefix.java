cfa83775498041951c808f0b84c7744a7de13122
Sumit1694 <Sumitrane198@gmail.com>
2026-06-18 00:05:46 +0530

    Longest common prefix

--- src/Strings/LongestCommonPrefix.java
+++ src/Strings/LongestCommonPrefix.java
@@ -1,19 +1,44 @@
 package Strings;
 public class LongestCommonPrefix
 {
-   public static void main(String [] args)
-   {
-       String [] arr = {"flower","flow","flight"};
+	static String longestCPrefix(String [] arr)
+	{
+		String prefix = arr[0];
 
-       String prefix = arr[0];
+		for(int i=1;i<arr.length;i++)
+		{
+			while(arr[i].indexOf(prefix)!=0)
+			{
+				prefix = prefix.substring(0, prefix.length()-1);
+			}
+		}
+		return prefix;
+	}
 
-       for(int i=1;i<arr.length;i++)
-       {
-          while(arr[i].indexOf(prefix)!=0)
-          {
-             prefix = prefix.substring(0, prefix.length()-1);
-          } 
-       }   
-     System.out.println(prefix);
-   }
+	static String longestCommonPref(String [] arr)
+	{
+		String prefix = arr[0];
+
+		for(int i=1;i<arr.length;i++)
+		{
+			while(!arr[i].startsWith(prefix))
+			{
+				prefix = prefix.substring(0, prefix.length()-1);
+
+				if(prefix.isEmpty())
+				{
+					return "";
+				}
+			}
+		}
+
+		return prefix;
+	}
+
+	public static void main(String [] args)
+	{
+		String [] arr = {"flower","flow","flight"};
+		String s = longestCommonPref(arr);
+        System.out.println(s);
+	}
 }
